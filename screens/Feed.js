9425a4b3348866f31fb41433f21bba9ef56c48c9
YuvrajSingh016 <72135408+YuvrajSingh016@users.noreply.github.com>
2021-12-07 12:20:19 +0530

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { FlatList, Text, View } from 'react-native';
+
+export default class Feed extends Component {
+    render() {
+        return (
+            <View>
+                <SafeAreaView style = {styles.droidSafeArea}/>
+                <View style = {styles.appTitle}>
+                    <View style = {styles.appIcon}>
+                        <Image
+                            source = {require("../assets/logo.png")}
+                            style = {styles.iconImage}
+                        ></Image>
+                    </View>
+                    <View style = {styles.appTitleTextContainer}>
+                        <Text style = {styles.appTitleText}>Spectagram</Text>
+                    </View>
+                </View>
+                <View style = {styles.cardContainer}>
+                    <FlatList
+                        keyExtractor = {this.keyExtractor}
+                        data = {props}
+                        renderIntem = {this.renderItem}
+                        />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "black"
+    },
+    droidSafeArea: {
+      marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appTitle: {
+      flex: 0.07,
+      flexDirection: "row"
+    },
+    appIcon: {
+      flex: 0.2,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    iconImage: {
+      width: "100%",
+      height: "100%",
+      resizeMode: "contain"
+    },
+    appTitleTextContainer: {
+      flex: 0.8,
+      justifyContent: "center"
+    },
+    appTitleText: {
+      color: "white",
+      fontSize: RFValue(28),
+    },
+    cardContainer: {
+      flex: 0.85
+    }
+  });
